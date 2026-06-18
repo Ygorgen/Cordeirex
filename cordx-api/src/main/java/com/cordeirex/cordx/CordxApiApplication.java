e0895da6477d4bb8ea22a2c1831be2d13c94ab90
Ygor de Araujo <140752592+Ygorgen@users.noreply.github.com>
2026-06-18 19:42:10 -0300

    Add files via upload

---
+++ cordx-api/src/main/java/com/cordeirex/cordx/CordxApiApplication.java
@@ -0,0 +1,13 @@
+package com.cordeirex.cordx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CordxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CordxApiApplication.class, args);
+	}
+
+}
---
+++ cordx-api/src/test/java/com/cordeirex/cordx/CordxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordeirex.cordx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CordxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
